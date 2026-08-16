77bdeb675608f11a80d69c9409feb26d35fd7841
Travis Wessman <traviswessman26@gmail.com>
2026-08-16 08:45:25 -0400

    chapter one tasks

---
+++ the_function/tasks.py
@@ -0,0 +1,17 @@
+
+def week_to_minutes(n: int) -> int:
+    '''number of minutes in n weeks'''
+    return n * 7 * 24 * 60
+
+def remainder(dividend, divisor):
+    '''find remainder without mod'''
+    floor_div = dividend // divisor
+    return dividend - (divisor * floor_div)
+
+def is_sum_divisible(numbers: list[int], divisor: int) -> bool:
+    '''tests whether sum of numbers is divisible by given number'''
+    if divisor == 0:
+        raise ValueError("Divisor can't be zero.")
+    
+    return sum(numbers) % divisor == 0
+
